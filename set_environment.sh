f0eb19b3bfedd4ed7e556bf29574c0842099f7e1
katarinazatkova <97286388+katarinazatkova@users.noreply.github.com>
2022-02-06 12:46:59 +0000

    Create set_environment.sh

---
+++ set_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# set geant4 environment
+source /home/kzatkova/Desktop/geant4/geant4.10.07.p03-install/bin/geant4.sh
+
+# set root environment
+source /home/kzatkova/Desktop/root/root-6.24.06/buildroot/bin/thisroot.sh
