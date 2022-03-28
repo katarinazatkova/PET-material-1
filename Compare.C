f427f30fb54cd0af4781eb33ea64d64fdf93b59b
katarinazatkova <97286388+katarinazatkova@users.noreply.github.com>
2022-03-28 16:36:19 +0100

    Update Compare.C

--- Compare.C
+++ Compare.C
@@ -11,6 +11,7 @@
 
 #include <iostream> 
 
+
 using namespace std;
 using namespace TMath;
 
@@ -85,42 +86,45 @@ void Compare(){
   // Plots to draw:
   // - Histogram of the observed (radionuclide) displacement
   // - Number of events against the deflection angle (check of the gaussian distribution)
-  // - Histogram of the hits in the X-Y plane
-  // - 3D Histogram of the hits in the X-Y plane
+  // - Histogram of the hits in the Y-Z plane
+  // - 3D Histogram of the hits in the Y-Z plane
   // - Spatial resolution against the detector length
   
   
-  TH1D * d = new TH1D("d",
-		      "Differences between hits with and without the non-collinearity(mm)",
-		      100, 0, 10); 
+  TH1D * d = new TH1D("",
+		      "Differences between hits with and without the non-collinearity (mm)",
+		      100, 0, 10);
   
-  TH1D * gausscheck = new TH1D("gausscheck",
+  TH1D * gausscheck = new TH1D("",  
 		      "Number of Events against deflection angle(radians)",
-		      100, 0, 0.1);
+		      100, 0, 10);
 
-  TH2F * h2_YZ = new TH2F("h2_YZ",
+  TH2F * h2_YZ = new TH2F("",
           "2D Histogram of the hits in the Y-Z plane",
-          100, -10, 10, 100, -10, 10);
+          100, -1000, 1000, 100, -400, 400); 
 
-  TH2F * h3_YZ = new TH2F("h3_YZ",
+  TH2F * h3_YZ = new TH2F("",
           "3D Histogram of the hits in the Y-Z plane",
-          100, -10, 10, 100, -10, 10);
+          100, -1000, 1000, 100, -400, 400);
   
 
   // NOTE: to obtain a plot of the spatial resolution against the detector length
   // uncomment parts of the code below
+  /*
+  const Int_t n = 45;//97;
+  Double_t length_vals[n];
+  Double_t res_vals[n];
 
-  //const Int_t n = 97;
-  //Double_t length_vals[n];
-  //Double_t res_vals[n];
+  for (int i = 0; i <= 44; ++i) { //96
+  Double_t min_range = (-1940/2) + 50 + 10*i;
+  Double_t max_range = - min_range;
+  length_vals[i] = 2*max_range;
+  */
 
-  //for (int i = 0; i <= 96; ++i) {
-  //Double_t min_range = (-1940/2) + 10*i;
+  // Obtaining resolution for specific detector size
+  //Double_t min_range = -1940/2 + 200;
   //Double_t max_range = - min_range;
-  //length_vals[i] = 2*max_range;
 
-  //Double_t min_range = -1940/2 + 100;//(-1940/2) + 10*i;
-  //Double_t max_range = - min_range;// min_range;
 
   for(Long64_t entry = 0; entry < nentries; entry++){
   
@@ -130,17 +134,17 @@ void Compare(){
     //Considering only the good events
     if ((E_det1 > Threshold) && (E_det2 > Threshold)){
       
-      // considering shorter detector lengths
-      //if (((z11 > min_range) && (z11 < max_range)) && ((z12 > min_range) && (z12 < max_range)) && ((z21 > min_range) &&  (z21 < max_range)) && ((z22 < min_range) && (z22 > max_range))){
+      // Considering shorter detector lengths
+      //if (((z11 > min_range) && (z11 < max_range)) && ((z12 > min_range) && (z12 < max_range)) && ((z21 > min_range) &&  (z21 < max_range)) && ((z22 > min_range) && (z22 < max_range))){
       
-      // considering oblique events
-      //if (((z11 < min_range) || (z11 > max_range)) && ((z12 < min_range) || (z12 > max_range)) && ((z21 < min_range) ||  (z21 > max_range)) && ((z22 < min_range) ||  (z22 > max_range))){
+      // Considering only highly oblique events at max 20 cm from the edge of the PET detector
+      if (((z11 < min_range) || (z11 > max_range)) && ((z12 < min_range) || (z12 > max_range)) && ((z21 < min_range) ||  (z21 > max_range)) && ((z22 < min_range) ||  (z22 > max_range))){
     
       d1 = Sqrt( pow((x11-x21),2) + pow((y11-y21),2) + pow((z11-z21),2) ); 
       d2 = Sqrt( pow((x12-x22),2) + pow((y12-y22),2) + pow((z12-z22),2) );
       displacement = d2 - d1;
       cout << " d = " << displacement << endl;
-      def_angle = asin(Sqrt(pow((y22),2)+ (pow((z22),2)))/100);
+      def_angle = asin(Sqrt(pow((y22),2) + (pow((z22),2)))/x22);
 
       //gamma 1
       cout << endl;
@@ -169,6 +173,7 @@ void Compare(){
       cout << " z21 = " << z21 << endl;
       cout << " z22 = " << z22 << endl;
 
+
       cout << "Distance d1 is: " << d1 << endl;
       cout << "Distance d2 is: " << d2 << endl;
       
@@ -180,34 +185,41 @@ void Compare(){
 
       h3_YZ->Fill(z22, y22, x22);
       
-    //}}
+    //}
   }}
   
-  Double_t stdev = d->GetStdDev();
-  Double_t FWHM = stdev*sqrt(8*log(2)); //or 2.3548*stdev
+  // Calculating spatial resolution from the histogram
+  Int_t bin = d->FindLastBinAbove(d->GetMaximum()/2);
+  Double_t FWHM = 2*(d->GetBinCenter(bin));
+  // Alternative way
+  //Double_t stdev = d->GetStdDev();
+  //Double_t FWHM = 2.3548*stdev;
   cout << endl << "FWHM(Spatial Resolution) = " << FWHM << endl;
-  //res_vals[i] = FWHM; 
+
   
   /*
-  // plot spatial resolution against length of the detector
+  res_vals[i] = FWHM; 
+  }
+  
+  // Plot spatial resolution against length of the detector
   TCanvas *rlc  = new TCanvas();
   TGraph *rl = new TGraph(n,length_vals,res_vals);
-  rl->SetTitle("Spatial resolution as a function of the detector length; Detector length (mm); Spatial resolution (mm)");
+  rl->SetTitle("Spatial resolution as a function of detector length; Detector length (mm); FWHM (mm)");
   rl->GetXaxis()->CenterTitle(true);
   rl->GetYaxis()->CenterTitle(true);
-  rl->SetLineColor(kBlue);
-  //rl->SetLineWidth(1504);
+  rl->SetLineColor(kAzure+2);
   rl->SetFillStyle(3005);
-  rl->Draw("p");
+  rl->SetMarkerStyle(2);
+  rl->Draw("APL");
   rlc->SaveAs("res_vs_length.pdf");
   */
 
-  
   TCanvas * c1 = new TCanvas();
-  d->SetTitle("Difference between the observed and the actual position of the radionuclide for oblique events; Difference in position (mm); Number of events");
+  d->SetTitle("Difference between the observed and the actual position of the radionuclide; Difference in position (mm); Number of events");
   d->GetXaxis()->CenterTitle(true);
   d->GetYaxis()->CenterTitle(true);
-  d->SetLineColor(kBlue);
+  d->SetFillColor(kAzure+1);
+  d->SetLineColor(kAzure+2);
   //gStyle->SetOptStat(0);
   d->Draw();
   c1->SaveAs("d.pdf");
@@ -216,26 +228,28 @@ void Compare(){
   gausscheck->SetTitle("Distribution of deflection angles; Deflection angle (radians); Number of events");
   gausscheck->GetXaxis()->CenterTitle(true);
   gausscheck->GetYaxis()->CenterTitle(true);
-  gausscheck->SetLineColor(kBlue);
+  gausscheck->SetFillColor(kAzure+1);
+  gausscheck->SetLineColor(kAzure+2);
   gausscheck->Draw();
   c2->SaveAs("gausscheck.pdf");
 
   TCanvas * c3 = new TCanvas();
-  h2_YZ->SetTitle("A histogram of the deposited energy in the detector in the Y-Z plane; Z position (mm); Y position (mm)");
+  h2_YZ->SetTitle("Histogram of the deposited energy in the detector in the Y-Z plane; Z position (mm); Y position (mm)");
   h2_YZ->GetXaxis()->CenterTitle(true);
   h2_YZ->GetYaxis()->CenterTitle(true);
   h2_YZ->SetContour(1000);
   h2_YZ->Draw("colz");   
-  c3->SaveAs("h2_XY.pdf");
+  c3->SaveAs("h2_YZ.pdf");
   
   TCanvas * c4 = new TCanvas();
-  h3_YZ->SetTitle("A histogram of the spatial distribution of the considered events; Z position (mm); Y position (mm); X position (mm)");
+  h3_YZ->SetTitle("Histogram of the spatial distribution of the considered events; Z position (mm); Y position (mm); X position (mm)");
   h3_YZ->GetXaxis()->CenterTitle(true);
   h3_YZ->GetYaxis()->CenterTitle(true);
   h3_YZ->GetZaxis()->CenterTitle(true);
   h3_YZ->Draw("surf2d");   
   c4->SaveAs("h3_YZ.pdf");
 
+  /*
   //Creating a new file that will contain all the plots
   TFile * plots_file = new TFile("plots.root", "RECREATE");
   plots_file->cd();
@@ -247,4 +261,6 @@ void Compare(){
 
 
   plots_file->Close();
+  */
+
 }
